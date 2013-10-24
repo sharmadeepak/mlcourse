8b39f54714c5cfb1fc7caf35d02ab273aed25ace
sharmaji <dksharma166@gmail.com>
2013-10-24 22:29:58 +0530

    gradientSolved

--- mlclass-ex1/gradientDescent.m
+++ mlclass-ex1/gradientDescent.m
@@ -16,7 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-while delta>0.01
+	
+
 	sum1=0;
 	sum2=0;
 	for i=1:m
@@ -25,7 +26,7 @@ while delta>0.01
 	endfor
 theta(1)=theta(1)-alpha*sum1/m;
 theta(2)=theta(2)-alpha*sum2/m;
-endwhile
+
 
 
 
