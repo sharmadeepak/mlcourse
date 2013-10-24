fc1753c62a64e8883189af11ba14afe73e2350e4
sharmaji <dksharma166@gmail.com>
2013-10-24 17:35:56 +0530

    gradientdecentChange

--- mlclass-ex1/gradientDescent.m
+++ mlclass-ex1/gradientDescent.m
@@ -16,6 +16,16 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+while delta>0.01
+	sum1=0;
+	sum2=0;
+	for i=1:m
+	sum1=sum1+theta(1)+theta(2)*X(i,2)-y(i);
+	sum2=sum2+(theta(1)+theta(2)*X(i,2)-y(i))*X(i,2);
+	endfor
+theta(1)=theta(1)-alpha*sum1/m;
+theta(2)=theta(2)-alpha*sum2/m;
+endwhile
 
 
 
