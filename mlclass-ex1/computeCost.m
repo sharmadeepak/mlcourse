08c72b02f5339dd194013d2ed27237dad86401a6
sharmaji <dksharma166@gmail.com>
2013-10-24 17:10:25 +0530

    computecost_change

--- mlclass-ex1/computeCost.m
+++ mlclass-ex1/computeCost.m
@@ -12,7 +12,11 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+for i=1:m
+tmp=theta(1)+theta(2)*X(i,2)-y(i);
+J=J+tmp*tmp;
+endfor
+J=J/(2*m);
 
 
 
